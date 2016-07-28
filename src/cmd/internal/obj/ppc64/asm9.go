e10a01e811c98b977692587566bd60df0c951404
Carlos Eduardo Seo <cseo@linux.vnet.ibm.com>
2016-07-28 15:43:30 -0300

    Remove ISA 3.0 instructions.

--- src/cmd/internal/obj/ppc64/asm9.go
+++ src/cmd/internal/obj/ppc64/asm9.go
@@ -1275,16 +1275,10 @@ func buildop(ctxt *obj.Link) {
 			opset (AVSHASIGMAW, r0)
 			opset (AVSHASIGMAD, r0)
 
-		case ALXV: /* lxvb16x, lxvd2x, lxvl, lxvll, lxvx, lxvdsx, lxvh8x, lxvw4x, lxvwsx */
-			opset (ALXVB16X, r0)
+		case ALXV: /* lxvd2x, lxvdsx, lxvw4x */
 			opset (ALXVD2X, r0)
-			opset (ALXVL, r0)
-			opset (ALXVLL, r0)
-			opset (ALXVX, r0)
 			opset (ALXVDSX, r0)
-			opset (ALXVH8X, r0)
 			opset (ALXVW4X, r0)
-			opset (ALXVWSX, r0)
 
 		case AAND: /* logical op Rb,Rs,Ra; no literal */
 			opset(AANDN, r0)
@@ -3851,24 +3845,12 @@ func oploadx(ctxt *obj.Link, a obj.As) uint32 {
 	case ALVSR:
 		return OPVCC(31, 38, 0, 0) /* lvsr */
 
-	case ALXVB16X:
-		return OPVCC(31, 876, 0, 0) /* lxvb16x */
 	case ALXVD2X:
 		return OPVCC(31, 844, 0, 0) /* lxvd2x */
-	case ALXVL:
-		return OPVCC(31, 269, 0, 0) /* lxvl */
-	case ALXVLL:
-		return OPVCC(31, 301, 0, 0) /* lxvll */
-	case ALXVX:
-		return OPVCC(31, 268, 0, 0) | 1<<6 /* lxvx */
 	case ALXVDSX:
 		return OPVCC(31, 332, 0, 0) /* lxvdsx */
-	case ALXVH8X:
-		return OPVCC(31, 812, 0, 0) /* lxvh8x */
 	case ALXVW4X:
 		return OPVCC(31, 780, 0, 0) /* lxvw4x */
-	case ALXVWSX:
-		return OPVCC(31, 364, 0, 0) /* lxvwsx */
 	}
 
 	ctxt.Diag("bad loadx opcode %v", obj.Aconv(a))
